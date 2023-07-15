a17d4e998f678f8445f2f57b44c636832edc95aa
Debasish Bhola <devop69@gmail.com>
2023-07-15 20:22:01 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView myListView = (ListView) findViewById(R.id.myListView);
+
+        ArrayList<String>myFamily = new ArrayList<String>();
+    }
+}
