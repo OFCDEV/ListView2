000a6777133e5f3b983496c2d810d276db20468d
Debasish Bhola <devop69@gmail.com>
2023-07-16 11:07:58 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -29,6 +32,16 @@ public class MainActivity extends AppCompatActivity {
         //And what this essentially allows us to do is to convert our array list, which we already have, into
         //
         //a different format.
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,myFamily);
+
+        myListView.setAdapter(arrayAdapter);
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                Log.i("Person tapped:", myFamily.get(position));
+
+            }
+        });
     }
 }
